5ec383e73f427533a49ac0898e5b63c12d8273d1
athira-nair20 <95172872+athira-nair20@users.noreply.github.com>
2022-03-01 19:16:07 +0530

    Create registrationform.java

---
+++ registrationform.java
@@ -0,0 +1,164 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.sql.*;
+public class Registration implements ActionListener
+{
+    JFrame frame;
+    String[] gender={"Male","Female"};
+    String[] coursename={"Linear Algebra And Calculus","Vector Calculus Differential Equations And Transforms","Engineering Physics A","Engineering Chemistry","Engineering Mechanics","Engineering Graphics","Basics Of Civil And Mechanical Engineering","Basics Of Electrical And Electronics Engineering","Engineering Physics Lab","Programming In C","Life Skills","Professional Communication"};
+    String[] courseid={"MAT101","MAT102","PHT100","CYT100","EST100","EST110","EST120","EST130","PHL120","EST102","HUN101","HUN102"};
+    String[] time={"FN","AN"};
+    JLabel studidLabel=new JLabel("STUDID");
+    JLabel nameLabel=new JLabel("NAME");
+    JLabel genderLabel=new JLabel("GENDER");
+    JLabel coursenameLabel=new JLabel("COURSENAME");
+    JLabel courseidLabel=new JLabel("COURSEID");
+    JLabel creditLabel=new JLabel("CREDIT");
+    JLabel passwordLabel=new JLabel("PASSWORD");
+    JLabel collegeLabel=new JLabel("COLLEGE");
+    JLabel centrenameLabel=new JLabel("CENTRENAME");
+    JLabel timeLabel=new JLabel("TIME");
+    JTextField nameTextField=new JTextField();
+    JComboBox genderComboBox=new JComboBox(gender);
+    JComboBox coursenameComboBox=new JComboBox(coursename);
+    JComboBox courseidComboBox=new JComboBox(courseid);
+    JTextField creditTextField=new JTextField(credit);
+    JPasswordField passwordField=new JPasswordField();
+    JTextField collegeTextField=new JTextField();
+    JTextField centrenameTextField=new JTextField();
+    JComboBox timeComboBox= new JComboBox(time);
+    JButton registerButton=new JButton("REGISTER");
+    JButton resetButton=new JButton("RESET");
+   
+    RegistrationForm()
+    {
+        createWindow();
+        setLocationAndSize();
+        addComponentsToFrame();
+        actionEvent();
+    }
+    public void createWindow()
+    {
+        frame=new JFrame();
+        frame.setTitle("Registration Form");
+        frame.setBounds(40,40,380,600);
+        frame.getContentPane().setBackground(Color.pink);
+        frame.getContentPane().setLayout(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+    }
+    public void setLocationAndSize()
+    {
+        studidLabel.setBounds(20,20,40,70);
+        nameLabel.setBounds(20,70,40,70);
+        genderLabel.setBounds(20,120,80,70);
+        coursenameLabel.setBounds(20,170,100,70);
+        courseidLabel.setBounds(20,220,100,70);
+        creditLabel.setBounds(20,270,100,70);
+        passwordLabel.setBounds(20,320,140,70);
+        centrenameLabel.setBounds(20,370,100,70);
+        collegeLabel.setBounds(20,420,100,70);
+        timeLabel.setBounds(20,470,100,70);
+        nameTextField.setBounds(180,43,165,23);
+        genderComboBox.setBounds(180,93,165,23);
+        coursenameComboBox.setBounds(180,143,165,23);
+        courseidComboBox.setBounds(180,193,165,23);
+        creditTextField.setBounds(180,243,165,23);
+        passwordField.setBounds(180,293,165,23);
+        collegeTextField.setBounds(180,343,165,23);
+        centrenameTextField.setBounds(180,393,165,23);
+        timeComboBox.setBounds(180,444,165,23);
+        registerButton.setBounds(70,500,100,35);
+        resetButton.setBounds(220,500,100,35);
+    }
+    public void addComponentsToFrame()
+    {
+        frame.add(nameLabel);
+        frame.add(genderLabel);
+        frame.add(coursenameLabel);
+        frame.add(courseidLabel);
+        frame.add(creditLabel);
+        frame.add(passwordLabel);
+        frame.add(collegeLabel);
+        frame.add(centrenameLabel);
+        frame.add(timeLabel);
+        frame.add(nameTextField);
+        frame.add(genderComboBox);
+        frame.add(coursenameComboBox);
+        frame.add(courseidComboBox);
+        frame.add(creditComboBox);
+        frame.add(passwordField);
+        frame.add(collegeTextField);
+        frame.add(centrenameTextField);
+        frame.add(timeComboBox);
+        frame.add(registerButton);
+        frame.add(resetButton);
+    }
+    public void actionEvent()
+    {
+        registerButton.addActionListener(this);
+        resetButton.addActionListener(this);
+    }
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==registerButton)
+        {
+            try {
+                //Creating Connection Object
+                Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/myDatabase","root","root");
+                //Preapared Statement
+                PreparedStatement Pstatement=connection.prepareStatement("insert into student values(?,?,?,?,?,?,?)");
+                //Specifying the values of it's parameter
+                Pstatement.setString(1,nameTextField.getText());
+                Pstatement.setString(2,genderComboBox.getSelectedItem().toString());
+                Pstatement.setString(3,coursenameComboBox.getSelectedItem().toString());
+                Pstatement.setString(4,courseidComboBox.getSelectedItem().toString());
+                Pstatement.setString(5,creditComboBox.getSelectedItem().toString());
+                Pstatement.setString(6,passwordField.getText());
+                Pstatement.setString(6,collegeTextField.getText());
+                Pstatement.setString(7,centrenameTextField.getText());
+                Pstatement.setString(8,timeComboBox.getSelectedItem().toString());
+                //Checking for the Password match
+                if(passwordField.getText().equalsIgnoreCase(confirmPasswordField.getText()))
+                {
+                    //Executing query
+                    Pstatement.executeUpdate();
+                    JOptionPane.showMessageDialog(null,"Data Registered Successfully");
+                }
+                else
+                {
+                    JOptionPane.showMessageDialog(null,"password did not match");
+                }
+ 
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+ 
+ 
+        }
+        if(e.getSource()==resetButton)
+        {
+            //Clearing Fields
+            nameTextField.setText("");
+            genderComboBox.setSelectedItem(" ");
+            coursenameComboBox.setSelectedItem(" ");
+            courseisComboBox.setSelectedItem(" ");
+            creditComboBox.setSelectedItem(" ");
+            passwordField.setText("");
+            passwordField.setText("");
+            collegeTextField.setText("");
+            centrenameTextField.setText("");
+            timeComboBox.setSelectedItem(" ");
+        }
+ 
+    }
+}
+
+public class Main {
+    public static void main(String[] args)
+    {
+        new RegistrationForm();
+    }
+ 
+ }
