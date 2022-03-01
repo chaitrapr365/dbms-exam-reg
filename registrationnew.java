fddac56306e7b5d07e89b7222dd05fb17d55e688
chaitrapr365 <92593443+chaitrapr365@users.noreply.github.com>
2022-03-02 01:03:37 +0530

    Update registrationnew.java

--- registrationnew.java
+++ registrationnew.java
@@ -2,14 +2,16 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 import java.sql.*;
+import javax.swing.ImageIcon;
 public class RegistrationForm implements ActionListener
 {
-    JFrame frame;
+    JFrame frame,f2,f1;
     String[] gender={"Male","Female"};
     String[] coursename={"Linear Algebra And Calculus","Vector Calculus Differential Equations And Transforms","Engineering Physics A","Engineering Chemistry","Engineering Mechanics","Engineering Graphics","Basics Of Civil And Mechanical Engineering","Basics Of Electrical And Electronics Engineering","Engineering Physics Lab","Programming In C","Life Skills","Professional Communication"};
     String[] courseid={"MAT101","MAT102","PHT100","CYT100","EST100","EST110","EST120","EST130","PHL120","EST102","HUN101","HUN102"};
-    String[] semester={"S1",S2","S3","S4","S5","S6","S7","S8"};
+    String[] semester={"S1","S2","S3","S4","S5","S6","S7","S8"};
     String[] time={"FN","AN"};
+    JLabel w=new JLabel("Click here for exam registration");
     JLabel studidLabel=new JLabel("STUDID");
     JLabel nameLabel=new JLabel("NAME");
     JLabel genderLabel=new JLabel("GENDER");
@@ -17,8 +19,8 @@ public class RegistrationForm implements ActionListener
     JLabel courseidLabel=new JLabel("COURSEID");
     JLabel creditLabel=new JLabel("CREDIT");
     JLabel passwordLabel=new JLabel("PASSWORD");
-    JLabel collegeLabel=new JLabel("COLLEGE");
-    JLabel semesterLabel=new JLabel("SEMESTER");
+    JLabel collegeLabel=new JLabel("SEMESTER");
+    JLabel semesterLabel=new JLabel("COLLEGE");
     JLabel centrenameLabel=new JLabel("CENTRENAME");
     JLabel timeLabel=new JLabel("TIME");
     JTextField studidTextField=new JTextField();
@@ -34,6 +36,12 @@ public class RegistrationForm implements ActionListener
     JComboBox timeComboBox= new JComboBox(time);
     JButton registerButton=new JButton("REGISTER");
     JButton resetButton=new JButton("RESET");
+    JButton enterButton=new JButton("ENTER");
+    /*
+    JLabel NewLabel = new JLabel();
+    NewLabel.setIcon(new ImageIcon("a"));
+    NewLabel.setBounds(0, 0, 2000, 800);
+    frame.add(NewLabel);*/
    
     RegistrationForm()
     {
@@ -45,13 +53,25 @@ public class RegistrationForm implements ActionListener
     public void createWindow()
     {
         frame=new JFrame();
+        f2=new JFrame();
+        f1=new JFrame();
+        f1.setVisible(true);
+	   f2.setVisible(false);
         frame.setTitle("Registration Form");
         frame.setBounds(40,40,380,700);
-        frame.getContentPane().setBackground(Color.pink);
+        f2.setBounds(40,40,380,700);
+        frame.getContentPane().setBackground(Color.gray);
         frame.getContentPane().setLayout(null);
-        frame.setVisible(true);
+        frame.setVisible(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
+        frame.setResizable(true);
+        //JLabel NewLabel = new JLabel();
+        //NewLabel.setIcon(new ImageIcon("info.gif"));
+        //NewLabel.setBounds(0, 0, 2000, 800);
+        //frame.add(NewLabel);
+        f1.setBounds(40,40,380,700);
+        f1.getContentPane().setBackground(Color.gray);
+        f1.getContentPane().setLayout(null);
     }
     public void setLocationAndSize()
     {
@@ -78,10 +98,12 @@ public class RegistrationForm implements ActionListener
         centrenameTextField.setBounds(180,493,165,23);
         timeComboBox.setBounds(180,553,165,23);
         registerButton.setBounds(70,600,100,35);
+        enterButton.setBounds(20,270,100,70);
         resetButton.setBounds(220,600,100,35);
     }
     public void addComponentsToFrame()
     {
+	   f1.add(w);
         frame.add(studidLabel);
         frame.add(nameLabel);
         frame.add(genderLabel);
@@ -106,33 +128,54 @@ public class RegistrationForm implements ActionListener
         frame.add(timeComboBox);
         frame.add(registerButton);
         frame.add(resetButton);
+
+	   f1.add(enterButton);
+
+
     }
     public void actionEvent()
     {
         registerButton.addActionListener(this);
         resetButton.addActionListener(this);
+        enterButton.addActionListener(this);
     }
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e){
+        
+        if(e.getSource()==enterButton)
+        {
+        	frame.setVisible(true);
+        }
         if(e.getSource()==registerButton)
         {
             try {
+                
                 //Creating Connection Object
-                Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/myDatabase","root","root");
+                Class.forName("oracle.jdbc.driver.OracleDriver");  
+                Connection connection=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","sys as sysdba","chaidb");
                 //Preapared Statement
-                PreparedStatement Pstatement=connection.prepareStatement("insert into student values(?,?,?,?,?,?,?)");
+                PreparedStatement Pstatement=connection.prepareStatement("insert into candidate values(?,?,?,?,?,?,?)");
                 //Specifying the values of it's parameter
-                Pstatement.setString(0,studidTextField.getText());
-                Pstatement.setString(1,nameTextField.getText());
-                Pstatement.setString(2,genderComboBox.getSelectedItem().toString());
-                Pstatement.setString(3,coursenameComboBox.getSelectedItem().toString());
+                Pstatement.setString(1,studidTextField.getText());
+                Pstatement.setString(2,nameTextField.getText());
+	           Pstatement.setString(3,semesterComboBox.getSelectedItem().toString());
                 Pstatement.setString(4,courseidComboBox.getSelectedItem().toString());
-                Pstatement.setString(5,creditTextField.getText());
+                Pstatement.setString(5,collegeTextField.getText());
                 Pstatement.setString(6,passwordField.getText());
-                Pstatement.setString(7,collegeTextField.getText());
-                Pstatement.setString(8,semesterComboBox.getSelectedItem().toString());
-                Pstatement.setString(9,centrenameTextField.getText());
-                Pstatement.setString(A,timeComboBox.getSelectedItem().toString());
-                //Checking for the Password match
+                Pstatement.setString(7,genderComboBox.getSelectedItem().toString());
+                /*//Pstatement.setString(4,coursenameComboBox.getSelecte7dItem().toString());
+                //Pstatement.setString(5,courseidComboBox.getSelectedItem().toString());
+   
+                //Pstatement.setString(6,creditTextField.getText());
+                Pstatement.setString(7,passwordField.getText());
+               // Pstatement.setString(8,collegeTextField.getText());
+                Pstatement.setString(9,semesterComboBox.getSelectedItem().toString());
+                Pstatement.setString(10,centrenameTextField.getText());
+                Pstatement.setString(11,timeComboBox.getSelectedItem().toString());
+                //Statement Pstatement=connection.createStatement();*/
+                f2.setVisible(true);
+//Checking for the Password match
+
+                JOptionPane.showMessageDialog(null,"You have been Registered!");
                 /*if(passwordField.getText().equalsIgnoreCase(confirmPasswordField.getText()))
                 {
                     //Executing query
@@ -144,7 +187,7 @@ public class RegistrationForm implements ActionListener
                     JOptionPane.showMessageDialog(null,"password did not match");
                 }*/
  
-            } catch (SQLException e1) {
+            } catch (Exception e1) {
                 e1.printStackTrace();
             }
  
@@ -166,13 +209,10 @@ public class RegistrationForm implements ActionListener
             timeComboBox.setSelectedItem(" ");
         }
  
-    }
-}
-
-public class Main {
-    public static void main(String[] args)
+    } 
+        public static void main(String[] args)
     {
         new RegistrationForm();
     }
- 
- }
+
+}
