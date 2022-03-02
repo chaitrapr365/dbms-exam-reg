39cd53fe44bb586484ff48b0a84510f750d8cd6d
chaitrapr365 <92593443+chaitrapr365@users.noreply.github.com>
2022-03-02 14:28:53 +0530

    Update registration.java

--- registration.java
+++ registration.java
@@ -1,248 +1,211 @@
 // CREATE TABLE COURSE(COURSEID VARCHAR(20) PRIMARY KEY,COURSENAME VARCHAR(50),CREDIT NUMBER(8));
 //CREATE TABLE CANDIDATE(STUDID VARCHAR(10) PRIMARY KEY,NAME VARCHAR(40),SEMESTER VARCHAR(10),COURSEID VARCHAR(20) REFERENCES COURSE,COLLEGE VARCHAR(70),PASSWORD VARCHAR(30));
 //CREATE TABLE EXAMDETAIL(STUDID VARCHAR(10) REFERENCES CANDIDATE,CENTRENAME VARCHAR(50),TIME VARCHAR(20));
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
 import java.sql.*;
-public class Registration implements ActionListener,ItemListener
+import javax.swing.ImageIcon;
+public class RegistrationF implements ActionListener
 {
-  
-  JFrame f1,f2,f3;
-  JLabel jlab1,jlab2,jlab3,jlab4,jlab5,jlab6,jlab7,jlab8,l1,l2;
-  JTextField tf1,tf2,tf3,tf4,tf5,tf6,tf7,t1,t2;
-  TextField pass;
-  JButton b1,b2,b3,b4;
-  JRadioButton jb1,jb2,jb3;
-  ButtonGroup bg;
-  Connection c;
-  Statement stmt;
-  ResultSet rs;
-  String s,name,studid,courseid,semester,year,course,stmt1;
-  Registration() throws Exception
-  {
-   Class.forName("org.postgresql.Driver");
-    c = DriverManager.getConnection("jdbc:postgresql://ziggy.db.elephantsql.com:5432/dgmvslal","dgmvslal","Z9rYgKjyec0JM2iKRWfF5GAtmbLM7lBH");
-    stmt = c.createStatement();
-
-    f1 = new JFrame("Home Page");
-
-    f1.setSize(600,600);
-
-    jlab1 = new JLabel("UNIVERSITY");
-    JPanel pane=new JPanel(); 
-    //pane.setBackground(Color.black);
-    //pane.setBounds(50,20,500,10); 
-    jlab1.setBounds(50,10,120,30);
-    f1.add(jlab1);
-    //f1.add(pane);
-
-    jlab2 = new JLabel("Click Here for Login:",JLabel.CENTER);
-
-    jlab2.setBounds(100,50,120,30);
-
-    f1.add(jlab2);
-        
-    JPanel panel=new JPanel();  
-	 //JPanel pane=new JPanel(); 
-     panel.setBounds(40,80,500,500);    
-     panel.setBackground(Color.blue); 
-        //pane.setBackground(Color.black);
-	 //pane.setBounds(0,80,500,10); 
-     l1 = new JLabel("USERNAME");    
-     l1.setBounds(100,100,120,30);    	
-     t1=new JTextField(15);
-        //b3.setBackground(Color.yellow);   
-     l2=new JLabel("PASSWORD");   
-     l2.setBounds(200,200,120,15);  
-     pass=new TextField(15);
-     pass.setEchoChar('*');
-        //b4.setBackground(Color.green);   
-     panel.add(l1);
-     panel.add(t1);
-     panel.add(l2); 
-     panel.add(pass); 
-        //f1.add(pane);
-     f1.add(panel);  
-     
-     f1.add(jlab1);
-
-     jlab2 = new JLabel("Click Here for Login:",JLabel.CENTER);
-
-     b1=new JButton("SIGNUP");
-     b2=new JButton("LOGIN");
-
-    b1.setBounds(100,300,120,30);
-    b2.setBounds(500,300,120,30);
-    panel.add(b1); 
-    panel.add(b2); 
-
-    b1.addActionListener(this);
-    b2.addActionListener(this);
-
-    f1.add(panel);  
-
-    f1.setLayout(null);
-    
-
-    f1.setVisible(true);
-
-    f1.setDefaultCloseOperation(f1.EXIT_ON_CLOSE);
-
-
-
-//FRAME 2 _LOGIN PAGE
-   JLabel d_name,d_;
-   //JTextField tf1,tf2,tf3,t1,t2;
-
-    f2 = new JFrame("LOGIN PAGE");
-
-    f2.setSize(600,400);
-    f2.setLayout(null);
-    f2.setVisible(false);
-
-    jlab3 = new JLabel("Welcome to registration!",JLabel.CENTER);
-    jlab3.setBounds(60,60,200,60);
-    
-    d_name= new JLabel("");
-    d_name.setBounds(60,100,200,60);
-    tf1 = new JTextField(45);
-    f2.add(jlab3);
-    f2.add(d_name);
-
-    f2.add(tf1);
-    jlab4 = new JLabel("studid");
-
-    jlab4.setBounds(60,60,220,40);
-    tf2 = new JTextField(45);
-
-    f2.add(jlab4);
-    f2.add(tf2);
-    jlab5 = new JLabel("Course");
-    jlab5.setBounds(60,60,240,30);
-    f2.add(jlab5);
-    tf4=new JTextField(45);
-    f2.add(tf4);
-    jlab6 = new JLabel("Courseid");
-    f2.add(jlab6);
-    tf5=new JTextField(45);
-    f2.add(tf5);
-    jlab7 = new JLabel("Semester");
-    f2.add(jlab7);
-    tf6=new JTextField(45);
-    f2.add(tf6);
-    jlab8 = new JLabel("Year");
-    f2.add(jlab8);
-    tf7=new JTextField(45);
-    f2.add(tf7);
-    jb1 = new JRadioButton("Java");
-    jb2 = new JRadioButton("C++");
-    jb3 = new JRadioButton("HTML");
-
-    bg = new ButtonGroup();
-
-    f2.add(jb1);
-
-    f2.add(jb2);
-
-    f2.add(jb3);
-
-    bg.add(jb1);
-
-    bg.add(jb2);
-
-    bg.add(jb3);
-
-    jb1.addActionListener(this);
-
-    jb2.addActionListener(this);
-
-    jb3.addActionListener(this);
-
-    tf3 = new JTextField(30);
-
-    f2.add(tf3);
-
-    b2 = new JButton("SUBMIT");
-    b2.setBounds(60,50,300,20);
-
-    f2.add(b2);
-
-    b2.addActionListener(this);
-  }
-  public void actionPerformed(ActionEvent ae)
-  {
-
-    s = ae.getActionCommand();
-
-    try
+    JFrame frame,f2,f1;
+    String[] gender={"Male","Female"};
+    String[] coursename={"Linear Algebra And Calculus","Vector Calculus Differential Equations And Transforms","Engineering Physics A","Engineering Chemistry","Engineering Mechanics","Engineering Graphics","Basics Of Civil And Mechanical Engineering","Basics Of Electrical And Electronics Engineering","Engineering Physics Lab","Programming In C","Life Skills","Professional Communication"};
+    String[] courseid={"MAT101","MAT102","PHT100","CYT100","EST100","EST110","EST120","EST130","PHL120","EST102","HUN101","HUN102"};
+    String[] semester={"S1","S2","S3","S4","S5","S6","S7","S8"};
+    String[] time={"FN","AN"};
+    JLabel w=new JLabel("Click here for exam registration");
+    JLabel studidLabel=new JLabel("STUDID");
+    JLabel nameLabel=new JLabel("NAME");
+    JLabel genderLabel=new JLabel("GENDER");
+    JLabel coursenameLabel=new JLabel("COURSENAME");
+    JLabel courseidLabel=new JLabel("COURSEID");
+    JLabel creditLabel=new JLabel("CREDIT");
+    JLabel passwordLabel=new JLabel("PASSWORD");
+    JLabel confirmpasswordLabel=new JLabel("CONFIRMPASSWORD");
+    JLabel collegeLabel=new JLabel("SEMESTER");
+    JLabel semesterLabel=new JLabel("COLLEGE");
+    JLabel centrenameLabel=new JLabel("CENTRENAME");
+    JLabel timeLabel=new JLabel("TIME");
+    JTextField studidTextField=new JTextField();
+    JTextField nameTextField=new JTextField();
+    JComboBox genderComboBox=new JComboBox(gender);
+    JComboBox coursenameComboBox=new JComboBox(coursename);
+    JComboBox courseidComboBox=new JComboBox(courseid);
+    JTextField creditTextField=new JTextField();
+    JPasswordField passwordField=new JPasswordField();
+    JPasswordField confirmPasswordField=new JPasswordField();
+    JTextField collegeTextField=new JTextField();
+    JComboBox semesterComboBox=new JComboBox(semester);
+    JTextField centrenameTextField=new JTextField();
+    JComboBox timeComboBox= new JComboBox(time);
+    JButton registerButton=new JButton("REGISTER");
+    JButton resetButton=new JButton("RESET");
+    JButton enterButton=new JButton("ENTER");
+    JScrollPane scroll=new JScrollPane(frame);
+    ImageIcon i1=new ImageIcon("examreg.png");
+    JLabel li = new JLabel(i1);
+   
+    RegistrationF()
     {
- 
-     if(s.equals("LOGIN"))
-
-      {
-        f2.setVisible(true);
-        String userid=t1.getText();
-        /* //FROM DETAILS TABLE?
-	 statfetch ="select * from -table name- where st_id='"+userid+"';";
-        rs = stmt.executeQuery(statfetch);
-        while(rs.next())
-        {
-          //as per details in table
-          String Name=(rs.getString("name"));
-          //..String course=(rs.getInt("phoneno"));
-          //..System.out.println(rs.getString("course"));
-        }
-         d_name.setText(Name);
-	  */
-      }
-else if(s.equals("SIGNUP"))
-      {/*
-        name = tf1.getText();
-        phoneno = tf2.getText();
-        course = tf3.getText();
-        stmt1 = "Insert into registration values('"+name+"','"+phoneno+"','"+course+"');";
-        int p = stmt.executeUpdate(stmt1);
-        stmt1 ="select * from registration where name = '"+name+"';";
-        rs = stmt.executeQuery(stmt1);
-        while(rs.next())
-        {
-          System.out.println(rs.getString("name"));
-          System.out.println(rs.getInt("phoneno"));
-          System.out.println(rs.getString("course"));
-        }
-        JOptionPane.showMessageDialog(f2,"Successfully Registered.","Alert",JOptionPane.WARNING_MESSAGE);
-        f2.dispose();
-        f1.dispose();*/
-      }
-   }
-  catch(Exception e)
-
-   {
-  
-   e.printStackTrace();
-      }
- 
+        createWindow();
+        setLocationAndSize();
+        addComponentsToFrame();
+        actionEvent();
     }
-    public void itemStateChanged(ItemEvent ie)
-  {
-/*
-    //s=ie.getStateChange();
- 
-    try
+    public void createWindow()
     {
-      //for checkbox events
-      }
-   catch(SQLException e)
-
-   {
-  
-   e.printStackTrace();
-      }
- */
+        frame=new JFrame();
+        f2=new JFrame();
+        f1=new JFrame();
+ 
+        f1.setVisible(true);
+	 f2.setVisible(false);
+        frame.setTitle("Registration Form");
+        frame.setBounds(40,40,420,700);
+        f2.setBounds(40,40,420,700);
+        frame.getContentPane().setBackground(Color.gray);
+        frame.getContentPane().setLayout(null);
+        frame.getContentPane().add(scroll,BorderLayout.CENTER);
+        frame.setVisible(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(true);
+        f1.setBounds(40,40,420,700);
+        li.setBounds(0,0,420,700);
+        f1.getContentPane().setBackground(Color.gray);
+        f1.getContentPane().setLayout(null);
     }
- public static void main(String args[]) throws Exception
- {
-   new Registration();
- }
+    public void setLocationAndSize()
+    {
+        w.setBounds(80,220,200,80);
+        studidLabel.setBounds(20,10,140,70);
+        nameLabel.setBounds(20,60,40,70);
+        genderLabel.setBounds(20,100,80,70);
+        coursenameLabel.setBounds(20,140,100,70);
+        courseidLabel.setBounds(20,180,100,70);
+        creditLabel.setBounds(20,220,100,70);
+        passwordLabel.setBounds(20,260,140,70);
+        confirmpasswordLabel.setBounds(20,300,140,70);
+        centrenameLabel.setBounds(20,340,100,70);
+        collegeLabel.setBounds(20,380,100,70);
+        semesterLabel.setBounds(20,420,100,70);
+        timeLabel.setBounds(20,460,100,70);
+        studidTextField.setBounds(180,33,165,23);
+        nameTextField.setBounds(180,73,165,23);
+        genderComboBox.setBounds(180,113,165,23);
+        coursenameComboBox.setBounds(180,153,165,23);
+        courseidComboBox.setBounds(180,193,165,23);
+        creditTextField.setBounds(180,233,165,23);
+        passwordField.setBounds(180,273,165,23);
+        confirmPasswordField.setBounds(180,313,165,23);
+        collegeTextField.setBounds(180,353,165,23);
+        semesterComboBox.setBounds(180,393,165,23);
+        centrenameTextField.setBounds(180,433,165,23);
+        timeComboBox.setBounds(180,473,165,23);
+        registerButton.setBounds(70,540,100,35);
+        enterButton.setBounds(120,270,100,70);
+        resetButton.setBounds(220,540,100,35);
+    }
+    public void addComponentsToFrame()
+    {
+	 f1.add(w);
+        f1.add(li);
+        frame.add(studidLabel);
+        frame.add(nameLabel);
+        frame.add(genderLabel);
+        frame.add(coursenameLabel);
+        frame.add(courseidLabel);
+        frame.add(creditLabel);
+        frame.add(passwordLabel);
+        frame.add(confirmpasswordLabel);
+        frame.add(collegeLabel);
+        frame.add(semesterLabel);
+        frame.add(centrenameLabel);
+        frame.add(timeLabel);
+        frame.add(studidTextField);
+        frame.add(nameTextField);
+        frame.add(genderComboBox);
+        frame.add(coursenameComboBox);
+        frame.add(courseidComboBox);
+        frame.add(creditTextField);
+        frame.add(passwordField);
+        frame.add(confirmPasswordField);
+        frame.add(collegeTextField);
+        frame.add(semesterComboBox);
+        frame.add(centrenameTextField);
+        frame.add(timeComboBox);
+        frame.add(registerButton);
+        frame.add(resetButton);
+	  f1.add(enterButton);
+
+
+    }
+    public void actionEvent()
+    {
+        registerButton.addActionListener(this);
+        resetButton.addActionListener(this);
+        enterButton.addActionListener(this);
+    }
+    public void actionPerformed(ActionEvent e){
+        try{
+        if(e.getSource()==enterButton)
+        {
+        	frame.setVisible(true);
+        }
+        if(e.getSource()==registerButton)
+        {
+                Class.forName("oracle.jdbc.driver.OracleDriver");  
+                Connection connection=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","sys as sysdba","chaidb");
+                PreparedStatement Pstatement=connection.prepareStatement("insert into candidate values(?,?,?,?,?,?,?)");
+                PreparedStatement P2statement=connection.prepareStatement("insert into examdetail values(?,?,?)");
+                  if(passwordField.getText().equalsIgnoreCase(confirmPasswordField.getText()))
+                     {
+                        JOptionPane.showMessageDialog(null,"Data Registered Successfully");
+                        Pstatement.setString(1,studidTextField.getText());
+                	    Pstatement.setString(2,nameTextField.getText());
+	                 Pstatement.setString(3,semesterComboBox.getSelectedItem().toString());
+                       Pstatement.setString(4,courseidComboBox.getSelectedItem().toString());
+                       Pstatement.setString(5,collegeTextField.getText());
+                       Pstatement.setString(6,passwordField.getText());
+                       Pstatement.setString(7,genderComboBox.getSelectedItem().toString());
+                 	   Pstatement.executeUpdate();
+                       P2statement.setString(1,studidTextField.getText());
+                       P2statement.setString(2,centrenameTextField.getText());
+                       P2statement.setString(3,timeComboBox.getSelectedItem().toString());
+                       P2statement.executeUpdate();
+                   }
+                else
+                   {
+                     JOptionPane.showMessageDialog(null,"Password does not match");
+                   }
+ 
+          } 
+
+          else if(e.getSource()==resetButton)
+          {
+            studidTextField.setText(" ");
+            nameTextField.setText("");
+            genderComboBox.setSelectedItem(" ");
+            coursenameComboBox.setSelectedItem(" ");
+            courseidComboBox.setSelectedItem(" ");
+            creditTextField.setText("");
+            passwordField.setText("");
+            confirmPasswordField.setText("");
+            collegeTextField.setText("");
+            semesterComboBox.setSelectedItem(" ");
+            centrenameTextField.setText("");
+            timeComboBox.setSelectedItem(" ");
+        }
+ 
+    } 
+      catch (Exception e1) {
+                e1.printStackTrace();
+            }
 }
+        public static void main(String[] args)
+    {
+        new RegistrationF();
+    }
+
+}
+
